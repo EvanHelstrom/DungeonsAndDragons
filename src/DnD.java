91205a1865e826823b8c1d633f893d78c33f39b7
OfficialPledge <plassmuhx42@aol.com>
2019-10-02 17:20:23 -0500

    Initial commit

---
+++ src/DnD.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class DnD {	
+	public static void main(String[] args) {
+		String sayings[] = {"Hello and welcome to my shop!","Hello traveler!","How can I help you?"};
+		int randomKey = (int) Math.round(Math.random()*2);
+		System.out.println(sayings[randomKey]);
+	}
+
+}
