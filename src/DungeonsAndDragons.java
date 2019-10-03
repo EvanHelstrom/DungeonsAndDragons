ad19e480046af901123fd352254e8fc5d4511022
OfficialPledge <plassmuhx42@aol.com>
2019-10-03 14:23:03 -0500

    Added things

--- src/DungeonsAndDragons.java
+++ src/DungeonsAndDragons.java
@@ -1,10 +1,13 @@
-
+import java.util.*;
 public class DungeonsAndDragons {
 
 	public static void main(String[] args) {
 		String sayings[] = {"Hello and welcome to my shop!","Hello traveler!","How can I help you?"};
 		int randomKey = (int) Math.round(Math.random()*2);
 		System.out.println(sayings[randomKey]);
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String name = scanner.nextLine();
 	}
 
 }
