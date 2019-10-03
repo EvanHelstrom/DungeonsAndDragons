38a9445e23bde935ff76b1ddae944e23a6b1bac6
OfficialPledge <plassmuhx42@aol.com>
2019-10-02 19:35:14 -0500

    Wooooo

--- src/DungeonsAndDragons.java
+++ src/DungeonsAndDragons.java
@@ -1,5 +1,6 @@
-import java.util.*;
-public class DnD {	
+
+public class DungeonsAndDragons {
+
 	public static void main(String[] args) {
 		String sayings[] = {"Hello and welcome to my shop!","Hello traveler!","How can I help you?"};
 		int randomKey = (int) Math.round(Math.random()*2);
